536ac73da4afc06d4dd9dcf9e461f323c88f0331
LF <marklf@163.com>
2016-05-18 10:30:04 +0800

    Initial Commit

---
+++ LFProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFProgressHUD
+//
+//  Created by LongFan on 16/5/18.
+//  Copyright © 2016年 Long Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFProgressHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFProgressHUD
+//
+//  Created by LongFan on 16/5/18.
+//  Copyright © 2016年 Long Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
